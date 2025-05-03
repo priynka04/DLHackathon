82208216105a731178db9b2c993294e3744def47
pihu26112005 <piyushkumar26november@gmail.com>
2025-05-04 03:57:00 +0530

    Integrate Google Generative AI into decisionAgents; update query handling in main.py

--- backend/agents/decisionAgents.py
+++ backend/agents/decisionAgents.py
@@ -3,20 +3,26 @@ from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
 from langchain.prompts.chat import ChatPromptTemplate
+from dotenv import load_dotenv
+import google.generativeai as genai
+
 
 load_dotenv()
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+api_key = os.getenv("GEMINI_API_KEY")
 
 embedder = HuggingFaceEmbeddings(
     model_name="intfloat/e5-base-v2",
     model_kwargs={"device": "cpu"}
 )
 
-llm = HuggingFaceEndpoint(
-    repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
-    temperature=0.7,
-    max_length=200
-)
+# llm = HuggingFaceEndpoint(
+#     repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
+#     temperature=0.7,
+#     max_length=200
+# )
+genai.configure(api_key=api_key)
+llm = genai.GenerativeModel("gemini-2.0-flash")
 
 relevance_prompt_template = ChatPromptTemplate.from_messages([
     ("system", 
@@ -35,12 +41,16 @@ relevance_prompt_template = ChatPromptTemplate.from_messages([
 
 def isQueryRelevantAgent(query: str) -> str:
     try:
-        formatted_prompt = relevance_prompt_template.format_messages(query=query)
-        response = llm.invoke(formatted_prompt).strip().lower()
+        # formatted_prompt = relevance_prompt_template.format_messages(query=query)
+        # response = llm.invoke(formatted_prompt).strip().lower()
+        formatted_messages = relevance_prompt_template.format_messages(query=query)
+        prompt_str = "\n\n".join([f"{msg.content}" for msg in formatted_messages])
+        response = llm.generate_content(prompt_str)
+        llm_response = response.text.strip().lower()
 
-        if "yes" in response:
+        if "yes" in llm_response:
             return "yes"
-        elif "no" in response:
+        elif "no" in llm_response:
             return "no"
         else:
             return "no"
